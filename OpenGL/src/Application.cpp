01796d36d3a929ff7e7ddb4416884c8eaeba7a34
unknown <omanovic.edo@gmail.com>
2019-09-07 16:48:59 +0200

    implemented first vertex and fragment shaders

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,7 +1,53 @@
-#include <iostream>
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	//error handling
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);//iv: integer, vector - fn wants a pointer
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		//char message[length]; //c++ wants a constant.. or need new.. heap alloc, so we'll use a trick
+		char* message = (char*)alloca(length * sizeof(char)); //allocates on stack dynamically
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " <<
+			(type == GL_VERTEX_SHADER ? "vertex" : "fragment") 
+			<< "shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) 
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	//linked, stored inside program, we can delete them
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 int main()
 {
@@ -50,6 +96,33 @@ int main()
 	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)8);
 	//for example if texture is 8 bytes offset - cast 8 into const void*
 
+	//vertex shader, fragment (pixel) shaders 90% of usage.. tessalation, geometry shaders but these are rare
+	//layout(location = 0) - is matching with index 0 of our glVertexAttribPointer
+	//we use vec2.. but GL needs vec4 and it knows how to cast it
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+
+	//color r,g,b alfa.. 0 is white 1 is black.. floats
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;"
+		"\n"
+		"void main()"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+		
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -63,15 +136,19 @@ int main()
 
 		//bound buffer will be drawn
 		glDrawArrays(GL_TRIANGLES, 0, 3); //3 vertices to be rendered, starting at zero
-
-		//Mogu napamet kucat svako slovo
-
+		//when we issue draw call - vertex shader gets called (once for each vertex), primary purpose to tell where vertex goes on screen
+		//then fragment shader called.. run once for each pixel rasterized (drawn) on screen - primary purp. to decide color of pixel(s)
+		//fragment shader needs to be really well optimized
+		//then we see result on the screen
+				
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 }
